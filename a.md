3f30c8c43cf513b1530af645a2cd31a47a5df568
Kahiro-M <k_matsu@riso.co.jp>
2019-09-26 14:24:54 +0900

    markdown output

---
+++ a.md
@@ -0,0 +1,16 @@
+対応がないt検定
+p value : 0.08055387210850919
+
+
+|service_user|service_data|  cv_user  |cv_data|
+|------------|-----------:|-----------|------:|
+|ME Ohashi   |           1|NB Orikasa |      0|
+|ME Tanaka   |           0|NB Takemoto|      1|
+|NB Tujino   |           0|ME Arimura |      1|
+|EL Hara     |           0|AR Inagawa |      0|
+|NB Fukumoto |           1|NB Enfbold |      1|
+|SU Ando     |           0|SU Sada    |      1|
+|SU Ohashi   |           1|SU Tominaga|      1|
+|SU Esumi    |           0|SU Ozawa   |      1|
+|AR Iijima   |           0|ME Komai   |      0|
+|ME Yoshida  |           0|ES Okawa   |      1|
--- tken.py
+++ tken.py
@@ -1,27 +1,50 @@
-﻿import numpy as np
+﻿#!/usr/bin/python
+# coding: UTF-8
+# -*- Coding: utf-8 -*-
+
+import numpy as np
 import pandas as pd
 import html_template as html
 import pytablewriter
 
 from scipy import stats
 
-print(html.header)
+# file open
+htmlfile = open('a.html','w')
+mdfile = open('a.md','w')
+
+
+htmlfile.write(html.header)
 
 df = pd.DataFrame({
-  'service_user' : ["ME大橋","ME田中良典","NB辻野","EL原","NB福本","SU安藤","SU大橋","SU江角","AR飯島","ME吉田"],
+  'service_user' : ["ME Ohashi","ME Tanaka","NB Tujino","EL Hara","NB Fukumoto","SU Ando","SU Ohashi","SU Esumi","AR Iijima","ME Yoshida"],
   'service_data' : [1,0,0,0,1,0,1,0,0,0],
-  'cv_user'      : ["NB折笠","NB武本","ME有村","AR稲川","NBエンフボルド","SU佐田","SU冨永","SU大澤","ME駒井","ES大川"],
+  'cv_user'      : ["NB Orikasa","NB Takemoto","ME Arimura","AR Inagawa","NB Enfbold","SU Sada","SU Tominaga","SU Ozawa","ME Komai","ES Okawa"],
   'cv_data'      : [0,1,1,0,1,1,1,1,0,1],
 })
 df2 = df.ix[:,['service_user','service_data','cv_user','cv_data']]
-print(df2.to_html())
+df2_html = df2.to_html()
+htmlfile.write(df2_html.encode("utf8"))
 
-print('対応がないt検定')
-print(stats.ttest_ind(df.service_data, df.cv_data, equal_var = False))
+htmlfile.write('<br>')
+htmlfile.write(html.footer)
 
-print('<br>')
-print(html.footer)
+
+mdfile.write('対応がないt検定')
+mdfile.write("\n")
+mdtable = stats.ttest_ind(df.service_data, df.cv_data, equal_var = False)
+mdfile.write('p value : ')
+mdfile.write(str(mdtable.pvalue))
+mdfile.write("\n")
+mdfile.write("\n")
+mdfile.write("\n")
 
 writer = pytablewriter.MarkdownTableWriter()
-writer.from_dataframe(df)
+writer.from_dataframe(df2)
+writer.stream = mdfile
 writer.write_table()
+
+
+# file close
+htmlfile.close()
+mdfile.close()
