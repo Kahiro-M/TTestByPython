8033a4d25b9e145d6b162b4b761a2e7e6b2dfcde
Kahiro-M <k_matsu@riso.co.jp>
2019-09-27 09:22:11 +0900

    FileOpenToWith

--- tken.py
+++ tken.py
@@ -12,7 +12,10 @@ from scipy import stats
 
 a_csvData = pd.read_csv("./A.csv",encoding="utf_8")
 b_csvData = pd.read_csv("./B.csv",encoding="utf_8")
-tTestResult = stats.ttest_ind(a_csvData.A_data, b_csvData.B_data, equal_var = False)
+
+csvDf = a_csvData.join(b_csvData)
+
+tTestResult = stats.ttest_ind(csvDf.A_data, csvDf.B_data, equal_var = False)
 
 df = pd.DataFrame({
   'service_user' : ["ME O","ME T","NB T","EL H","NB F","SU A","SU O","SU E","AR I","ME Y"],
@@ -23,41 +26,29 @@ df = pd.DataFrame({
 tTestResult = stats.ttest_ind(df.service_data, df.cv_data, equal_var = False)
 
 # html
-htmlFile = open('a.html','w')
-htmlFile.write(html.header)
 
-df2 = df.ix[:,['service_user','service_data','cv_user','cv_data']]
-df2_html = df2.to_html()
-htmlFile.write(df2_html.encode("utf8"))
-
-htmlFile.write('<br>')
-
-df2_html = a_csvData.to_html()
-htmlFile.write(df2_html.encode("utf8"))
-
-df2_html = b_csvData.to_html()
-htmlFile.write(df2_html.encode("utf8"))
-
-htmlFile.write(html.footer)
+with open('a.html', mode='w') as htmlFile:
+  htmlFile.write(html.header)
+  df2 = df.ix[:,['service_user','service_data','cv_user','cv_data']]
+  df2_html = df2.to_html()
+  htmlFile.write(df2_html.encode("utf8"))
+  htmlFile.write('<br>')
+  htmlFile.write(a_csvData.to_html().encode("utf8"))
+  htmlFile.write(b_csvData.to_html().encode("utf8"))
+  htmlFile.write(html.footer)
 
 
 # markdown
-mdFile = open('a.md','w')
+with open('a.md', mode='w') as mdFile:
+  mdFile.write('対応がないt検定')
+  mdFile.write("\n")
+  mdFile.write('p value : ')
+  mdFile.write(str(tTestResult.pvalue))
+  mdFile.write("\n")
+  mdFile.write("\n")
+  mdFile.write("\n")
 
-mdFile.write('対応がないt検定')
-mdFile.write("\n")
-mdFile.write('p value : ')
-mdFile.write(str(tTestResult.pvalue))
-mdFile.write("\n")
-mdFile.write("\n")
-mdFile.write("\n")
-
-writer = pytablewriter.MarkdownTableWriter()
-writer.from_dataframe(df2)
-writer.stream = mdFile
-writer.write_table()
-
-
-# file close
-htmlFile.close()
-mdFile.close()
+  writer = pytablewriter.MarkdownTableWriter()
+  writer.from_dataframe(df2)
+  writer.stream = mdFile
+  writer.write_table()
