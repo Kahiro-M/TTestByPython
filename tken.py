0290080344c85cdcad350c98a05bad63b30d7d84
Kahiro-M <k_matsu@riso.co.jp>
2019-09-27 09:37:06 +0900

    PValueOutput

--- tken.py
+++ tken.py
@@ -16,39 +16,37 @@ b_csvData = pd.read_csv("./B.csv",encoding="utf_8")
 csvDf = a_csvData.join(b_csvData)
 
 tTestResult = stats.ttest_ind(csvDf.A_data, csvDf.B_data, equal_var = False)
+resultStrPVal = "P value : "+str(tTestResult.pvalue)
 
-df = pd.DataFrame({
-  'service_user' : ["ME O","ME T","NB T","EL H","NB F","SU A","SU O","SU E","AR I","ME Y"],
-  'service_data' : [1,0,0,0,1,0,1,0,0,0],
-  'cv_user'      : ["NB O","NB T","ME A","AR I","NB E","SU S","SU T","SU O","ME K","ES O"],
-  'cv_data'      : [0,1,1,0,1,1,1,1,0,1],
-})
-tTestResult = stats.ttest_ind(df.service_data, df.cv_data, equal_var = False)
+if tTestResult.pvalue<0.05:
+  resultStrTTest = "有意差あり"
+else:
+  resultStrTTest = "有意差なし"
 
-# html
-
-with open('a.html', mode='w') as htmlFile:
-  htmlFile.write(html.header)
-  df2 = df.ix[:,['service_user','service_data','cv_user','cv_data']]
-  df2_html = df2.to_html()
-  htmlFile.write(df2_html.encode("utf8"))
-  htmlFile.write('<br>')
-  htmlFile.write(a_csvData.to_html().encode("utf8"))
-  htmlFile.write(b_csvData.to_html().encode("utf8"))
-  htmlFile.write(html.footer)
+# html output
+with open("result.html", mode="w") as fileObj:
+  fileObj.write(html.header)
+  fileObj.write("対応がないt検定")
+  fileObj.write("<br>")
+  fileObj.write(resultStrPVal)
+  fileObj.write("<br>")
+  fileObj.write(resultStrTTest)
+  fileObj.write("<br>")
+  fileObj.write("<br>")
+  fileObj.write(csvDf.to_html().encode("utf8"))
+  fileObj.write(html.footer)
 
 
-# markdown
-with open('a.md', mode='w') as mdFile:
-  mdFile.write('対応がないt検定')
-  mdFile.write("\n")
-  mdFile.write('p value : ')
-  mdFile.write(str(tTestResult.pvalue))
-  mdFile.write("\n")
-  mdFile.write("\n")
-  mdFile.write("\n")
-
+# markdown output
+with open("result.md", mode="w") as fileObj:
+  fileObj.write("対応がないt検定")
+  fileObj.write("\n")
+  fileObj.write(resultStrPVal)
+  fileObj.write("\n")
+  fileObj.write(resultStrTTest)
+  fileObj.write("\n")
+  fileObj.write("\n")
   writer = pytablewriter.MarkdownTableWriter()
-  writer.from_dataframe(df2)
-  writer.stream = mdFile
+  writer.from_dataframe(csvDf)
+  writer.stream = fileObj
   writer.write_table()
