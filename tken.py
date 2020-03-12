83be1618937f56e7facbb005d72de3ea27cfce0f
Kahiro-M <k_matsu@riso.co.jp>
2020-03-12 16:11:57 +0900

    windows対応、markdown出力機能削除

--- tken.py
+++ tken.py
@@ -5,10 +5,25 @@
 import numpy as np
 import pandas as pd
 import csv
-import html_template as html
 import pytablewriter
 
 from scipy import stats
+
+html_header = """
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  </head>
+  <body>
+"""
+
+html_footer = """
+  </body>
+</html>
+"""
 
 a_csvData = pd.read_csv("./A.csv",encoding="utf_8")
 b_csvData = pd.read_csv("./B.csv",encoding="utf_8")
@@ -24,8 +39,8 @@ else:
   resultStrTTest = "有意差なし"
 
 # html output
-with open("result.html", mode="w") as fileObj:
-  fileObj.write(html.header)
+with open("result.html", mode="w", encoding="utf_8") as fileObj:
+  fileObj.write(html_header)
   fileObj.write("対応がないt検定")
   fileObj.write("<br>")
   fileObj.write(resultStrPVal)
@@ -33,20 +48,5 @@ with open("result.html", mode="w") as fileObj:
   fileObj.write(resultStrTTest)
   fileObj.write("<br>")
   fileObj.write("<br>")
-  fileObj.write(csvDf.to_html().encode("utf8"))
-  fileObj.write(html.footer)
-
-
-# markdown output
-with open("result.md", mode="w") as fileObj:
-  fileObj.write("対応がないt検定")
-  fileObj.write("\n")
-  fileObj.write(resultStrPVal)
-  fileObj.write("\n")
-  fileObj.write(resultStrTTest)
-  fileObj.write("\n")
-  fileObj.write("\n")
-  writer = pytablewriter.MarkdownTableWriter()
-  writer.from_dataframe(csvDf)
-  writer.stream = fileObj
-  writer.write_table()
+  fileObj.write(csvDf.to_html())
+  fileObj.write(html_footer)
