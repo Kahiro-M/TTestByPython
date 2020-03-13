9e37a8add3c68e7b2d769ee1083b5ef2cd020e15
Kahiro-M <k_matsu@riso.co.jp>
2020-03-13 14:14:40 +0900

    サンプルデータセットを平均身長に変更、LetterValuePlotとSwarmPlot追加

--- tTest.py
+++ tTest.py
@@ -37,10 +37,29 @@ html_footer = """
 
 a_csvData = pd.read_csv("./A.csv",encoding="utf_8")
 b_csvData = pd.read_csv("./B.csv",encoding="utf_8")
+
+tTestResult = stats.ttest_ind(a_csvData.A_data, b_csvData.B_data, equal_var = False)
+resultStrPVal = "P value : "+str(tTestResult.pvalue)
+
+if tTestResult.pvalue<0.05:
+  resultStrTTest = "有意差あり"
+else:
+  resultStrTTest = "有意差なし"
+
 anlyDf=pd.DataFrame({
   "Group":np.concatenate([np.tile("A",len(a_csvData.A_data)),(np.tile("B",len(b_csvData.B_data)))]),
   "Data":np.concatenate([a_csvData.A_data,b_csvData.B_data]),
   })  
+sns.swarmplot(x="Group",y="Data",data=anlyDf)
+plt.title("Swarm plot")  
+plt.savefig("swarm.png")
+plt.clf()
+
+sns.boxenplot(x="Group",y="Data",data=anlyDf,)
+plt.title("Letter value plot")  
+plt.savefig("lv.png")
+plt.clf()
+
 sns.boxplot(x="Group",y="Data",data=anlyDf)
 plt.title("Box plot")  
 plt.savefig("box.png")
@@ -58,18 +77,10 @@ plt.title("Histgram")
 plt.savefig("hist.png")
 plt.clf()
 
-sns.barplot(x="Group",y="Data",data=anlyDf)
-plt.title("Bar plot")  
+sns.barplot(x="Group",y="Data",data=anlyDf,capsize=0.1)
+plt.title("Bar plot(avg.)")  
 plt.savefig("bar.png")
 plt.clf()
-
-tTestResult = stats.ttest_ind(a_csvData.A_data, b_csvData.B_data, equal_var = False)
-resultStrPVal = "P value : "+str(tTestResult.pvalue)
-
-if tTestResult.pvalue<0.05:
-  resultStrTTest = "有意差あり"
-else:
-  resultStrTTest = "有意差なし"
 
 # html output
 with open("result.html", mode="w", encoding="utf_8") as fileObj:
@@ -85,9 +96,11 @@ with open("result.html", mode="w", encoding="utf_8") as fileObj:
   fileObj.write(b_csvData.to_html())
   fileObj.write("<br>")
 
-  fileObj.write("<img src='box.png'>")
+  fileObj.write("<img src='swarm.png'>")
   fileObj.write("<img src='violin.png'>")
   fileObj.write("<img src='hist.png'>")
+  fileObj.write("<img src='lv.png'>")
+  fileObj.write("<img src='box.png'>")
   fileObj.write("<img src='bar.png'>")
 
   fileObj.write(html_footer)
